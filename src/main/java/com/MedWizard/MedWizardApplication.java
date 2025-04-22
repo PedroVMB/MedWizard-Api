57d3e58485cf129b5535aaaab178d3871d9b2154
PedroVMB <pedromb.dev@gmail.com>
2025-04-22 13:30:51 -0300

    feat: adicionando configuracoes do docker com imagem do mysql e no application.properties

---
+++ src/main/java/com/MedWizard/MedWizardApplication.java
@@ -0,0 +1,13 @@
+package com.MedWizard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedWizardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedWizardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MedWizard/MedWizardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MedWizard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedWizardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
